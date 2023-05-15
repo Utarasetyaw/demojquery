68b77a0d99f53140cbd7ae4db9c34220de7bc7cf
Utara.sw <tarazekage@gmail.com>
2023-05-15 19:45:23 +0700

    fix index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tugas Pemweb</title>
+    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -33,5 +33,41 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="jquery.js"></script>
+
+    <script>
+        $(document).ready(function(){
+            $('#myForm').submit(function(event){
+                event.preventDefault(); // mencegah halaman form berpindah ke dokumen
+                var form_data = $(this).serialize(); // mengambil data form dan membuatnya menjadi string
+                $.ajax({
+                url: 'controller.php', // URL script PHP untuk memproses data form
+                type: 'POST',
+                data: form_data,
+                success: function(response){
+                    console.log(response);
+
+                    // Mengubah response dari JSON menjadi array
+                    // var myArray = JSON.parse(response); selalu error kalau menggunakan JSON.parse()
+                    var myArray = response;
+                        
+                    $('#tabel').remove(); //untuk menghapus tabel setiap kali form disubmit
+                    // Membuat tabel untuk menampilkan array
+                    var table = '<table class="table table-striped table-hover mt-5" id="tabel"><thead><tr><th class="fw-medium">Index</th><th class="fw-medium">Value</th></tr></thead><tbody>';
+                    for (var i = 0; i < myArray.length; i++) {
+                    table += '<tr><td class="">' + i + '</td><td class="">' + myArray[i] + '</td></tr>';
+                    }
+                    table += '</tbody></table>';
+
+                    // Menampilkan tabel di bawah form dengan efek fadeIn
+                    // $('#myForm').after(table).next().hide().fadeIn();
+                    $('#myForm').after(table).next().hide().fadeIn(2000);
+                },
+                error: function(xhr, status, error){
+                    console.log(xhr.responseText); // tampilkan error di console browser
+                }
+                });
+            });
+        });
+    </script>
 </body>
 </html>
